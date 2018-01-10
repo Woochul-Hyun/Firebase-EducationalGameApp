e9f2929ea129f9fc9f1ff955d2b8d3e4ffdb4e5c
Woochul-Hyun <woochul.hyun@my.jcu.edu.au>
2018-01-10 21:25:36 +0800

    functions to load category at fragment category

--- app/src/main/java/com/example/woochulhyun/educationalgameapp/ViewHolder/CategoryViewHolder.java
+++ app/src/main/java/com/example/woochulhyun/educationalgameapp/ViewHolder/CategoryViewHolder.java
@@ -1,8 +1,39 @@
 package com.example.woochulhyun.educationalgameapp.ViewHolder;
 
+import android.support.v7.view.menu.MenuView;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.woochulhyun.educationalgameapp.Interface.ItemClickListener;
+import com.example.woochulhyun.educationalgameapp.R;
+
 /**
  * Created by WOOCHUL HYUN on 2018-01-10.
  */
 
-public class CategoryViewHolder {
+public class CategoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView category_name;
+    public ImageView category_image;
+
+    private ItemClickListener itemClickListener;
+
+    public CategoryViewHolder(View itemView) {
+        super(itemView);
+        category_image = (ImageView)itemView.findViewById(R.id.category_image);
+        category_name = (TextView)itemView.findViewById(R.id.category_name);
+
+        itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        itemClickListener.onClick(view,getAdapterPosition(),false);
+    }
 }
