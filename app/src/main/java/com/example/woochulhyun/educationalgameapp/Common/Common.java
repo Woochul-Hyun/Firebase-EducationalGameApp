f632755c0a2e4e0a80550eaa3b0828beea08e816
Woochul-Hyun <woochul.hyun@my.jcu.edu.au>
2018-01-13 22:57:28 +0800

    Common function

---
+++ app/src/main/java/com/example/woochulhyun/educationalgameapp/Common/Common.java
@@ -0,0 +1,19 @@
+package com.example.woochulhyun.educationalgameapp.Common;
+
+import com.example.woochulhyun.educationalgameapp.Model.Question;
+import com.example.woochulhyun.educationalgameapp.Model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by WOOCHUL HYUN on 2018-01-13.
+ */
+
+
+
+public class Common {
+    public static String categoryId;
+    public static User currentUser;
+    public static List<Question> questionList = new ArrayList<>();  //global variable list of Question
+}
