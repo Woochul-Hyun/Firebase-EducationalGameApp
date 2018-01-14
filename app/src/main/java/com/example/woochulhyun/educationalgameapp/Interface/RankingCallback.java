01e21d20de79e07d88264354f3d9f1cb6e14ccd8
Woochul-Hyun <woochul.hyun@my.jcu.edu.au>
2018-01-15 02:44:31 +0800

    ranking interface

---
+++ app/src/main/java/com/example/woochulhyun/educationalgameapp/Interface/RankingCallback.java
@@ -0,0 +1,9 @@
+package com.example.woochulhyun.educationalgameapp.Interface;
+
+/**
+ * Created by WOOCHUL HYUN on 2018-01-15.
+ */
+
+public interface RankingCallback<T> {
+    void callBack(T ranking);
+}
