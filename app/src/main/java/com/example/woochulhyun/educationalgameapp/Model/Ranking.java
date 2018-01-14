a6648b3d576f129f0ba41a8cade842d0767afbda
Woochul-Hyun <woochul.hyun@my.jcu.edu.au>
2018-01-15 02:43:48 +0800

    ranking model

---
+++ app/src/main/java/com/example/woochulhyun/educationalgameapp/Model/Ranking.java
@@ -0,0 +1,34 @@
+package com.example.woochulhyun.educationalgameapp.Model;
+
+/**
+ * Created by WOOCHUL HYUN on 2018-01-15.
+ */
+
+public class Ranking {
+    private String userName;
+    private long score;
+
+    public Ranking() {
+    }
+
+    public Ranking(String userName, long score) {
+        this.userName = userName;
+        this.score = score;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public long getScore() {
+        return score;
+    }
+
+    public void setScore(long score) {
+        this.score = score;
+    }
+}
